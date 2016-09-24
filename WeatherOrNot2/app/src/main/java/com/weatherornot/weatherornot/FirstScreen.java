3bcd59e81ab376d0d42772c5ce704469848a0bd5
DongJunJin <d29jin@uwaterloo.ca>
2016-09-24 02:41:05 -0400

    Edit1

--- WeatherOrNot2/app/src/main/java/com/weatherornot/weatherornot/FirstScreen.java
+++ WeatherOrNot2/app/src/main/java/com/weatherornot/weatherornot/FirstScreen.java
@@ -1,13 +1,24 @@
 package com.weatherornot.weatherornot;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
 public class FirstScreen extends AppCompatActivity {
+
+    LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_first_screen);
     }
+
+
 }
