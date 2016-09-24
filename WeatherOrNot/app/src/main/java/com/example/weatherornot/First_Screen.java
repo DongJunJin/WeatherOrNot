fdf5486ba836b7b506bd9fac591e2778f1ec1913
DongJunJin <d29jin@uwaterloo.ca>
2016-09-24 01:32:38 -0400

    rewind

--- WeatherOrNot/app/src/main/java/com/example/weatherornot/First_Screen.java
+++ WeatherOrNot/app/src/main/java/com/example/weatherornot/First_Screen.java
@@ -2,6 +2,7 @@ package com.example.weatherornot;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,5 +43,9 @@ public class First_Screen extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void onMapSearch (View view){
+
     }
 }
